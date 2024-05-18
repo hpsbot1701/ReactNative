7edb3fa51cf99dfa3a9910db29f46a8f4c6d70ee
Himanshu Pratap Singh <130280134+hpsbot1701@users.noreply.github.com>
2024-05-18 16:46:41 +0530

    Create WebViewPackage.js

---
+++ WebViewPackage.js
@@ -0,0 +1,21 @@
+
+import React, { Component, useEffect, useState } from 'react';
+import {WebView} from 'react-native-webview'
+import{Button, Text,View, StyleSheet, TextInput, FlatList, SectionList, TouchableHighlight, TouchableOpacity, ActivityIndicator, Modal, Pressable, StatusBar}from 'react-native'
+
+
+const App = ()=> {
+    
+    return(
+   
+     <WebView source={{uri:"https://www.jiocinema.com/"}}/>
+       
+   
+     
+
+    )
+  }
+
+
+
+export default App;
